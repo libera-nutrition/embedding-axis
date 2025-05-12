da50794354e08010a4ab0f0dc5d8d6c54224f782
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2025-05-12 01:24:12 +0000

    Use diskcache

--- src/embeddingaxis/util/openai_.py
+++ src/embeddingaxis/util/openai_.py
@@ -1,13 +1,12 @@
 import openai
-import json
-from pathlib import Path
 
-from embeddingaxis.config import PERSISTENT_CACHE_EMBEDDINGS
+from embeddingaxis.config import DISKCACHE, PERSISTENT_CACHE_EMBEDDINGS
 from embeddingaxis.util.dotenv_ import load_dotenv
 
 load_dotenv()
 
 
+@DISKCACHE.memoize(tag="get_embedding")
 def get_embedding(text: str) -> list[float]:
     """Return the embedding vector for the given text."""
     try:
@@ -16,5 +15,6 @@ def get_embedding(text: str) -> list[float]:
         pass
 
     response = openai.embeddings.create(input=text, model="text-embedding-3-large")
+    # Note: If model is updated, refresh PERSISTENT_CACHE_EMBEDDINGS and delete diskcache.
     print(f"Got embedding for {text!r}.")
     return response.data[0].embedding

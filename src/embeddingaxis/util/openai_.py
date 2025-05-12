aa97a227eb00e6c9a9de1c91fee688e65993539a
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2025-05-12 00:58:33 +0000

    Update openai_ util module to merge get_cached_embedding functionality into get_embedding function

--- src/embeddingaxis/util/openai_.py
+++ src/embeddingaxis/util/openai_.py
@@ -7,18 +7,14 @@ from embeddingaxis.util.dotenv_ import load_dotenv
 
 load_dotenv()
 
+
 def get_embedding(text: str) -> list[float]:
     """Return the embedding vector for the given text."""
-    response = openai.embeddings.create(input=text, model="text-embedding-3-large")
-    print(f"Got embedding for {text!r}.")
-    return response.data[0].embedding
-
-
-def get_cached_embedding(text: str) -> list[float]:
-    """Return the embedding vector for the given text, using cache if available."""
     try:
         return PERSISTENT_CACHE_EMBEDDINGS[text]
     except KeyError:
         pass
 
-    return get_embedding(text)
+    response = openai.embeddings.create(input=text, model="text-embedding-3-large")
+    print(f"Got embedding for {text!r}.")
+    return response.data[0].embedding

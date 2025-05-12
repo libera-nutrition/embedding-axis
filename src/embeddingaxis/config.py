311ceb2c55399381c4d51751c25560a1a0f7752c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2025-05-12 01:23:42 +0000

    Add diskcache

--- src/embeddingaxis/config.py
+++ src/embeddingaxis/config.py
@@ -1,6 +1,7 @@
-import datetime
 import json
 from pathlib import Path
+
+import diskcache
 
 from embeddingaxis.util.dotenv_ import load_dotenv
 
@@ -10,8 +11,8 @@ CWD: Path = Path.cwd()
 PACKAGE_PATH: Path = Path(__file__).parent
 PACKAGE_NAME: str = PACKAGE_PATH.name
 
-DISKCACHE_SIZE_GiB: int = 10
-GiB: int = 1024**3
+_GiB: int = 1024**3
+DISKCACHE: diskcache.FanoutCache = diskcache.FanoutCache(directory=str(PACKAGE_PATH / ".diskcache"), shards=8, timeout=10, size_limit=5 * _GiB)
 PERSISTENT_CACHE_EMBEDDINGS_STRINGS: set[str] = {
     "weight loss",
     "weight gain",
